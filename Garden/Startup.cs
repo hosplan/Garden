70b40788efe865731ff15089897f2a353b75771c
THKim <xogh1994@naver.com>
2021-04-05 23:16:20 +0900

    Nuget패키지 추가(페이지 새로고침시 바로 적용)

--- Garden/Startup.cs
+++ Garden/Startup.cs
@@ -34,7 +34,10 @@ namespace Garden
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddControllersWithViews();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
