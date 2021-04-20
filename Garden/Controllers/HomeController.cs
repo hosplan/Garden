e96ebd79d5db41dc4ffd2e3a9e92bf7b3cf5d8f7
THKim <xogh1994@naver.com>
2021-04-21 00:29:32 +0900

    update

--- Garden/Controllers/HomeController.cs
+++ Garden/Controllers/HomeController.cs
@@ -1,25 +1,36 @@
-﻿using Garden.Models;
+﻿using Garden.Data;
+using Garden.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Garden.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
-        }
+            _context = context;
 
-        public IActionResult Index()
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
+            string loginedUserId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(loginedUserId))
+            {
+                return Redirect("/Identity/Account/Login");
+            }
             return View();
         }
 
